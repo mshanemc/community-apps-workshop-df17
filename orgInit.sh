4d487602d9b4a1442c0b8c7eeb09ab35bcecf206
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-09-25 12:30:56 -0500

    date adjuster in orgInit script

--- orgInit.sh
+++ orgInit.sh
@@ -4,4 +4,5 @@ sfdx force:org:create -f config/project-scratch-def.json -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:data:tree:import -p data/masterImportPlan.json
+sfdx force:apex:execute -f scrips/setup.cls
 sfdx force:org:open
