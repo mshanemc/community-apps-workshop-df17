99d21c6d8f305d9bbb9a50d69908060513d5a715
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-11-01 18:42:47 -0500

    1 day on scratch, plus .ignore

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sfdx force:org:create -f config/project-scratch-def.json -s
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:data:tree:import -p data/masterImportPlan.json
