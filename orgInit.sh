8d43a73da2cdb5173968550394919271ecf87ee3
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-11-03 19:33:46 -0700

    type on scripts!

--- orgInit.sh
+++ orgInit.sh
@@ -4,5 +4,5 @@ sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:data:tree:import -p data/masterImportPlan.json
-sfdx force:apex:execute -f scrips/setup.cls
+sfdx force:apex:execute -f scripts/setup.cls
 sfdx force:org:open
